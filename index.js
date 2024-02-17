354d3c9329bec405e94e63b4f181cf0d93d3c0d2
Tryfonov Denys <54479637+TrifonovDenys@users.noreply.github.com>
2024-02-17 22:04:06 +0200

    -

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
 //   return replace;
 // }
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.overTheRoad = void 0;
+exports.lastSurvivor = void 0;
 // htmlspecialchars("<h2>Hello World</h2>")
 // function noBoringZeros(n: number): number {
 //   for (let i = String(n).length - 1; i > 0; i--)
@@ -229,16 +229,25 @@ exports.overTheRoad = void 0;
 //   }, []))
 // }
 // console.log(maxGap([-498, 0]));
-function overTheRoad(address, n) {
-    const l = [];
-    const r = [];
-    for (let i = 1; i <= n * 2; i += 1) {
-        i % 2 !== 0 ? l.push(i) : r.push(i);
-    }
-    const rI = l.findIndex(el => el === address);
-    console.log(rI);
-    // return r.reverse()[rI]
-    return r[r.length - rI - 1];
+// export function overTheRoad(address: number, n: number): number {
+//   const l: number[] = []
+//   const r: number[] = []
+//   for (let i = 1; i <= n * 2; i += 1) {
+//     i % 2 !== 0 ? l.push(i) : r.push(i)
+//   }
+//   const rI = l.findIndex(el => el === address)
+//   console.log(rI);
+//   // return r.reverse()[rI]
+//   return r[r.length - rI - 1]
+// }
+// console.log(overTheRoad(1, 3));
+function lastSurvivor(letters, coords) {
+    return coords.reduce((acc, el) => {
+        // acc.splice(el, 1)
+        return acc.splice(el, 1);
+    }, letters.split(''))[0];
 }
-exports.overTheRoad = overTheRoad;
-console.log(overTheRoad(1, 3));
+exports.lastSurvivor = lastSurvivor;
+// console.log(lastSurvivor('kbc', [0, 1])); //b
+console.log(lastSurvivor('foiflxtpicahhkqjswjuyhmypkrdbwnmwbrrvdycqespfvdviucjoyvskltqaqirtjqulprjjoaiagobpftywabqjdmiofpsr', [8, 59, 52, 93, 21, 40, 88, 85, 59, 10, 82, 18, 74, 59, 51, 47, 75, 49, 23, 56, 1, 33, 39, 33, 34, 44, 25, 0, 51, 25, 36, 32, 57, 10, 57, 12, 51, 55, 24, 55, 31, 49, 6, 15, 10, 48, 27, 29, 38, 30, 35, 42, 23, 32, 9, 39, 39, 36, 8, 29, 2, 33, 14, 3, 13, 25, 9, 25, 18, 10, 1, 2, 20, 8, 2, 11, 5, 7, 0, 10, 10, 8, 12, 3, 5, 1, 7, 7, 5, 1, 4, 0, 4, 0, 0, 1])); //z
+// console.log(['z', 'b', 'k'].splice(2));
--- index.tsx
+++ index.tsx
@@ -291,16 +291,27 @@
 // console.log(maxGap([-498, 0]));
 
 
-export function overTheRoad(address: number, n: number): number {
-  const l: number[] = []
-  const r: number[] = []
-  for (let i = 1; i <= n * 2; i += 1) {
-    i % 2 !== 0 ? l.push(i) : r.push(i)
-  }
-  const rI = l.findIndex(el => el === address)
-  console.log(rI);
-  // return r.reverse()[rI]
-  return r[r.length - rI - 1]
-}
+// export function overTheRoad(address: number, n: number): number {
+//   const l: number[] = []
+//   const r: number[] = []
+//   for (let i = 1; i <= n * 2; i += 1) {
+//     i % 2 !== 0 ? l.push(i) : r.push(i)
+//   }
+//   const rI = l.findIndex(el => el === address)
+//   console.log(rI);
+//   // return r.reverse()[rI]
+//   return r[r.length - rI - 1]
+// }
 
-console.log(overTheRoad(1, 3));
+// console.log(overTheRoad(1, 3));
+
+export function lastSurvivor(letters: string, coords: number[]): string {
+  return coords.reduce((acc: string[], el: number) => {
+    acc.splice(el, 1)
+    return acc
+  }, letters.split(''))[0]
+}
+// console.log(lastSurvivor('kbc', [0, 1])); //b
+console.log(lastSurvivor('foiflxtpicahhkqjswjuyhmypkrdbwnmwbrrvdycqespfvdviucjoyvskltqaqirtjqulprjjoaiagobpftywabqjdmiofpsr', [8, 59, 52, 93, 21, 40, 88, 85, 59, 10, 82, 18, 74, 59, 51, 47, 75, 49, 23, 56, 1, 33, 39, 33, 34, 44, 25, 0, 51, 25, 36, 32, 57, 10, 57, 12, 51, 55, 24, 55, 31, 49, 6, 15, 10, 48, 27, 29, 38, 30, 35, 42, 23, 32, 9, 39, 39, 36, 8, 29, 2, 33, 14, 3, 13, 25, 9, 25, 18, 10, 1, 2, 20, 8, 2, 11, 5, 7, 0, 10, 10, 8, 12, 3, 5, 1, 7, 7, 5, 1, 4, 0, 4, 0, 0, 1])); //z
+
+// console.log(['z', 'b', 'k'].splice(2)); 
