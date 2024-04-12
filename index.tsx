477087267725739fb7ee04799e24dcc2975fe782
Tryfonov Denys <54479637+TrifonovDenys@users.noreply.github.com>
2024-04-12 23:05:23 +0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1249,3 +1249,18 @@ export function dative(word: string): string | undefined {
   const back = 'aáoóuú'.split('')
   return front.some(el => word.includes(el)) ? `${word}nak` : `${word}nek`
 }
+
+export function jumpingNumber(n: number) {
+  let arr: number[] = String(n).split('')
+    .map(num => Number(num))
+
+  if (arr.length === 1) {
+    return 'Jumping!!'
+  }
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] - arr[i + 1] !== 1 && arr[i + 1] - arr[i] !== 1) {
+      return 'Not!!'
+    }
+  }
+  return 'Jumping!!'
+}
