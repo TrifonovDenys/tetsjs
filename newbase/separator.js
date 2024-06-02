4e0c3e5b9c7eaf7c8f7518d4da3d7abf9a6a72fe
Tryfonov Denys <54479637+TrifonovDenys@users.noreply.github.com>
2024-06-02 20:53:33 +0300

    sdfsdf

--- newbase/separator.js
+++ newbase/separator.js
@@ -1,11 +1,33 @@
-const { fullNames } = require('./dneprusers');
+// const { fullNames } = require('./dneprusers');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-const namesSeparotor = (str) => {
-    const arrNames = str.split(`\n`);
+const fullNames = `Lytbc Nhbajyjd   Ybrjkftdbx
+Денис Трифонов   Николаевич`;
+const csvWriter = createCsvWriter({
+    path: 'fullNames.csv',
+    header: [
+        { id: 'Призвище', title: 'Призвище' },
+        { id: 'Ім`я', title: 'Ім`я' },
+        { id: 'По-батькові', title: 'По-батькові' },
+    ],
+    encoding: 'utf-8',
+});
+
+function replaceMultipleSpaces(str) {
+    return str.trim().replace(/\s{2,}/g, ' ');
+}
+
+const namesSeparotor = (str, oneLine = true) => {
+    // oneLine = 0 || false || '' || undefined || null;
+    const arrNames = replaceMultipleSpaces(str).split(`\n`);
     const secondName = arrNames.map((name) => name.split(' ')[0]).join('\n');
     const firstName = arrNames.map((name) => name.split(' ')[1]).join('\n');
     const lastName = arrNames.map((name) => name.split(' ')[2]).join('\n');
-    console.log(lastName);
+
+    return secondName;
 };
 
-namesSeparotor(fullNames);
+const base = namesSeparotor(fullNames);
+// console.log(base);
+
+csvWriter.writeRecords(base).then(() => console.log('The CSV file was written successfully'));
